05673bd13ea5299f28075f6b8248e41ab43b9cb0
Alfredo <alfredocortez12god@gmail.com>
2024-10-21 10:16:01 -0400

    Add files via upload

---
+++ AlfredoCortez.py
@@ -0,0 +1,16 @@
+# Datos personales
+nombre = "Alfredo Alejandro Cortez Fernandez" 
+edad = 20
+semestre = 4
+carrera = "Ingenieria Sistemas Informáticos" 
+grupo = "C-Force"
+proyecto = "Estudio de Tatuajes Kraken"
+
+# Imprimir datos
+print("Datos Personales:")
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad} años")
+print(f"Semestre: {semestre}to semestre")
+print(f"Carrera: {carrera}")
+print(f"Grupo: {grupo}")
+print(f"Proyecto: {proyecto}")
