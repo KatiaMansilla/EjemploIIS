955c6f29c8f2a8797ba1583ce9dc4d93e6e55adf
lbz480 <zleandro067@gmail.com>
2024-10-21 09:45:39 -0400

    Create leandroDatos.py

---
+++ leandroDatos.py
@@ -0,0 +1,5 @@
+print("Nombre: Leandro Manuel Bolaños Zabala")
+print("Edad: 19 años")
+print("Fecha de Nacimiento: 2005-03-26")
+print("Grupo: C force")
+print("Proyecto: Estudio de Tatuajes Kraken")
