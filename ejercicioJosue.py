23137df9aee824dee7eced21a4811b92200739b1
josutatsu <131466495+josutatsu@users.noreply.github.com>
2024-10-21 10:40:31 -0400

    Create ejercicioJosue.py

---
+++ ejercicioJosue.py
@@ -0,0 +1,4 @@
+print("mi nombre es josue joel quisbert totora")
+print("tengo 19 anios")
+print("estoy en la carrera de ingenieria en sistemnas informaticos")
+print("mi proyecto integrador es el de la casa de la imilla")
