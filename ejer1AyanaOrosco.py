da0a67dcabbd572bdb736278bc13f2888e4882cf
ShakiraOrosco <shakiraog2005@gmail.com>
2024-10-21 10:39:27 -0400

    Update ejer1AyanaOrosco.py

--- ejer1AyanaOrosco.py
+++ ejer1AyanaOrosco.py
@@ -10,11 +10,11 @@ def son_numeros_amigos(num1, num2):
     """Verifica si num1 y num2 son números amigos."""
     return suma_divisores_propios(num1) == num2 and suma_divisores_propios(num2) == num1
 
-# Leer dos números del usuario
+#LEER DOS NÚMEROS DEL USUARIO
 numero1 = int(input("Ingrese el primer número: "))
 numero2 = int(input("Ingrese el segundo número: "))
 
-# Verificar si son números amigos
+#VERIFICAR SI SON NUMEROS AMIGOS
 if son_numeros_amigos(numero1, numero2):
     print(f"{numero1} y {numero2} son números amigos.")
 else:
