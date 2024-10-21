aa67a2e1290082d8303f06767efd58fda6c6037d
Oliv-dev2908 <oliverradictos@gmail.com>
2024-10-21 10:32:46 -0400

    Update EjercicioMamaniOliver.py

--- EjercicioMamaniOliver.py
+++ EjercicioMamaniOliver.py
@@ -8,11 +8,11 @@ def suma_divisores_propios(num):
 def son_amigos(num1, num2):
     return suma_divisores_propios(num1) == num2 and suma_divisores_propios(num2) == num1
  
-# Solicitar dos números al usuario
+# SOLICITAR 2 NÚMEROS AL USUARIO 
 num1 = int(input("Ingresa el primer número: "))
 num2 = int(input("Ingresa el segundo número: "))
  
-# Verificar si son amigos
+# VERIFICAR SI SON AMIGOS 
 if son_amigos(num1, num2):
     print(f"{num1} y {num2} son números amigos.")
 else:
