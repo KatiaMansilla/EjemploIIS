2a5faff4b024cf6b41e55ef94211de02e27924a7
Andres Baldiviezo <ChubiBF>
2024-10-21 10:16:34 -0400

    ejercicio1 AWBF

---
+++ ejercicioAndresBaldiviezo.py
@@ -0,0 +1,21 @@
+#ANDRES WILFREDO BALDIVIEZO FARFAN
+def suma_divisores_propios(n):
+    suma = 0
+    for i in range(1, n):
+        if n % i == 0:
+            suma += i
+    return suma
+
+def sonAmigos(num1, num2):
+    
+    return suma_divisores_propios(num1) == num2 and suma_divisores_propios(num2) == num1
+
+
+a = int(input("Introduce el numero 1: "))
+b = int(input("Introduce el numero 2: "))
+
+# Verificar si son amigos
+if sonAmigos(a, b):
+    print(f"{a} y {b} son amigos")
+else:
+    print(f"{a} y {b} no son amigos")
